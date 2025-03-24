8bb65d9e6e2a019d674f13b2e2996311f70f0fd8
remysedlak <remysedlak@gmail.com>
2025-03-24 17:18:19 -0400

    Refactor Intro component layout for improved responsiveness and spacing

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 function Intro() {
     return (
-        <div className="flex items-center justify-center 
-                flex-col text-center pt-10 pb-6">
-            <h1 className="block lg:hidden text-4xl md:text-7xl mb-3 
-                    md:mb-6 font-bold">Remy Sedlak</h1>
-            <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row justify-between items-center">
-                <div className=" flex flex-col justify-around pr-5">
+        <div className="flex items-center justify-center flex-col text-center pt-10 pb-6">
+            <h1 className="block lg:hidden text-4xl md:text-7xl mb-3 md:mb-6 font-bold">Remy Sedlak</h1>
+            <div className="p-2 flex flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row justify-around items-center space-y-6 lg:space-y-0 lg:space-x-12">
+                <img src="/assets/me.jpg" alt="Remy Sedlak" className="w-48 h-48 rounded-full" />
+                <div className="flex flex-col justify-around pr-5">
                     <p className="text-base md:text-xl mb-3 font-medium text-center md:text-left">
-                    Innovating at the crossroads of psychology and technology 
-                    <br/>to create impactful digital experiences.
+                        Innovating at the crossroads of psychology and technology 
+                        <br/>to create impactful digital experiences.
                     </p>
                     <p className="text-base md:text-lg mb-3 max-w-xl text-center md:text-left">
                         Currently pursuing a B.S. in Computer Science and 
@@ -22,9 +21,6 @@ function Intro() {
                         engagement.
                     </p>
                 </div>
-                <img src="/assets/me.jpg" alt="Remy Sedlak" className="w-48 h-48 rounded-lg ml-6" />
-                
-                    
             </div>
         </div>
     );
