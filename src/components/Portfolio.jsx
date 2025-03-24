9e2490588d7022197050480ee2acac4d1ad7a762
remysedlak <remysedlak@gmail.com>
2025-03-24 17:27:46 -0400

    Remove unused image and enhance Portfolio component with a Title for better structure

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import portfolio from '../data/portfolio';
 import PortfolioItem from './PortfolioItem';
-
+import Title from './Title';
 function Portfolio() {
     return (
+        <>
+        <div className="flex items-center justify-center flex-col text-center">
+        <Title>Projects</Title>
+        </div>
         <div className="flex flex-col md:flex-row items-center justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {portfolio.map((project, index) => (
@@ -17,6 +21,7 @@ function Portfolio() {
                 ))}
             </div>
         </div>
+        </>
     );
 }
 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Title({ children, id }) {
    return (
       <h1 id={id && id}
-         className="text-2xl font-bold underline 
+         className="text-2xl font-bold 
          underline-offset-8 decoration-4 mb-5 
          text-gray-900 dark:text-gray-900">
          {children}
