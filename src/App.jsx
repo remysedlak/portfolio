a6fb28a0f1437f327fb536f0960b68fcf0222a31
remysedlak <remysedlak@gmail.com>
2025-03-24 17:29:06 -0400

    nav fixes

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,12 @@ function App() {
     <>
       <div className="text-lg p-2 rounded-md width-screen bg-blue-25 scroll-smooth">
         <div className="width-full mx-auto scroll-smooth">
-          <nav className="hidden lg:flex justify-between items-center p-4 scroll-smooth">
+          <nav className="hidden lg:flex justify-between items-center p-4 scroll-smooth mx-4">
             <div className="text-2xl font-bold text-gray-800">Hi, I am Remy Sedlak!</div>
             <div className="flex space-x-4">
               <button onClick={() => scrollToSection('portfolio')} className="text-gray-800 hover:text-gray-600 scroll-smooth">Portfolio</button>
               <button onClick={() => scrollToSection('timeline')} className="text-gray-800 hover:text-gray-600 scroll-smooth">Timeline</button>
-              <a href="https://remymane.com/" className="text-gray-800 hover:text-gray-600">My Music</a>
+              {/* <a href="https://remymane.com/" className="text-gray-800 hover:text-gray-600">My Music</a> */}
               <button onClick={() => scrollToSection('contact')} className="text-gray-800 hover:text-gray-600 scroll-smooth">Contact</button>
             </div>
           </nav>
