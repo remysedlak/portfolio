4f52c82127bbdea51418d4c2f91bc159309db076
remysedlak <remysedlak@gmail.com>
2025-03-24 17:36:53 -0400

    timeline margin fixes, looked too tight on mobile

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,7 +4,7 @@ import TimelineItem from './TimelineItem';
 import Title from './Title';
 function Timeline() {
     return (
-        <div className="flex flex-col md:flex-row justify-center my-20">
+        <div className="flex flex-col md:flex-row justify-center my-20 mx-4">
             <div className="w-full md:w-7/12 relative">
             <Title>Timeline</Title>
                 <ul className="space-y-10">
