42dda31bc1fb36d25ad90f589b57a550f9c61a04
remysedlak <remysedlak@gmail.com>
2025-03-25 20:20:35 -0400

    add resume button and fix greeting text in navigation

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,11 @@ function App() {
     <>
       <div className="text-lg p-2 rounded-md width-screen bg-white scroll-smooth">
         <div className="width-full mx-auto scroll-smooth">
+          <hr/>
           <nav className="hidden lg:flex justify-between items-center p-4 scroll-smooth mx-4">
-            <div className="text-2xl font-bold text-gray-800">Hi, I am Remy Sedlak!</div>
+            <div className="text-2xl font-bold text-gray-800">Hi, I'm Remy Sedlak!</div>
             <div className="flex space-x-4">
+              <button onClick={() => window.open('https://drive.google.com/file/d/1-gW3kevUAv6ImxSWIZgw-VO9A38-KFCk/view', '_blank')} className="text-gray-800 hover:text-gray-600">My Resume</button>
               <button onClick={() => scrollToSection('portfolio')} className="text-gray-800 hover:text-gray-600 scroll-smooth">Portfolio</button>
               <button onClick={() => scrollToSection('timeline')} className="text-gray-800 hover:text-gray-600 scroll-smooth">Timeline</button>
               {/* <a href="https://remymane.com/" className="text-gray-800 hover:text-gray-600">My Music</a> */}
