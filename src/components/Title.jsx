aca75ac96dfbf8bdf57fb54d1a8f4028a32f5cca
remysedlak <remysedlak@gmail.com>
2025-04-01 21:18:43 -0400

    removed uneeded dark tags

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ function Title({ children, id }) {
       <h1 id={id && id}
          className="text-2xl font-bold 
          underline-offset-8 decoration-4 my-5
-         text-gray-900 dark:text-gray-900">
+         text-gray-900">
          {children}
       </h1>
    )
