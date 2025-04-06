ab653426b693505e9cfb3ec28c790d113a6b4277
remysedlak <remysedlak@gmail.com>
2025-04-06 11:38:08 -0400

    refactor: replace sidebar SVG assets and update image source in App component

--- src/App.jsx
+++ src/App.jsx
@@ -31,9 +31,9 @@ function App() {
         <div className={`hidden md:block pl-2 pt-2 ${sideBar ? 'ml-48' : ``}`}>
           <button onClick={toggleSidebar}>
             <img
-              src={isDarkMode ? `./assets/sidebar-left-svgrepo-com(1).svg` : `./assets/sidebar-left-svgrepo-com(1).svg`}
+              src={isDarkMode ? `./assets/sidebar-black.svg` : `./assets/sidebar-black.svg`}
               alt="Hide or Show Navigation"
-              className={"opacity-30 hover:opacity-50 dark:hover:opacity-100 w-10 h-10"}
+              className={"opacity-30 hover:opacity-50 dark:opacity-30 dark:hover:opacity-10 w-10 h-10"}
             />
           </button>
         </div>
