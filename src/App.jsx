116ddc6bd24da4c183c9a88e117de380d77ab4af
remysedlak <remysedlak@gmail.com>
2025-06-15 01:15:01 -0400

    opacity change for background

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function App() {
           
           <div
             style={{ backgroundImage: `url(/assets/cartographer.png)`}}
-            className="absolute inset-0 bg-repeat opacity-10 transition-opacity duration-500 ease-in-out transition-all duration-100 mix-blend-multiply pointer-events-none z-0"
+            className="absolute inset-0 bg-repeat opacity-20 transition-opacity duration-500 ease-in-out transition-all duration-100 mix-blend-multiply pointer-events-none z-0"
           />
 
           <div id="intro">
